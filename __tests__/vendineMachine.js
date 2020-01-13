56dbfb7c865273e1badbd7b9c399f0d6d71ca202
jose <jmvr37@hotmail.com>
2020-01-13 02:47:53 -0800

    add a function for the tests

--- __tests__/vendineMachine.js
+++ __tests__/vendineMachine.js
@@ -1,9 +1,24 @@
-const vendingMachine = require("../lib/test1");
-describe("vending machine", () => {
-  describe("if there's not enough coins to purchase that item", () => {
-    it("it should return false", () => {
-      const result = vendingMachine();
-      expect(result).toEqual();
+const vendingMachine = require("../lib/vendineMachine");
+const Inventory = require("../lib/inventoryMachine");
+const changeInventory = require("../lib/changeMachine");
+const vendingM = new vendingMachine(Inventory, changeInventory);
+
+describe("Vending Machine:", () => {
+  describe("buy a new product", () => {
+    describe("When selection = 'A1' with exact change ", () => {
+      it("should return coke with no change", () => {
+        expect(vendingM.dispenseProduct("A1", 1.5)).toEqual("coke");
+      });
+    });
+    describe("When selection = 'A1' but more than the existence inventory ", () => {
+      it("not enough pruducts", () => {
+        expect(vendingM.dispenseProduct("A1", 11)).toThrow("error");
+      });
+    });
+    describe("When selection = 'B1' whit exact change ", () => {
+      it("it should return sprite with no change", () => {
+        expect(vendingM.dispenseProduct("B1", 1.25)).toEqual("sprite");
+      });
     });
   });
 });
