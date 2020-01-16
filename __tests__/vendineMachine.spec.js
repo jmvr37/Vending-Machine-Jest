a003c8df650d2eae88e20bf5a5e96156a7747bbb
jose <jmvr37@hotmail.com>
2020-01-15 19:29:29 -0800

    fix some issues with the restockproduct function

--- __tests__/vendineMachine.spec.js
+++ __tests__/vendineMachine.spec.js
@@ -16,39 +16,38 @@ describe("Vending Machine:", () => {
       });
     });
 
-    describe("When selection = 'B1' whit exact change ", () => {
+    describe("When selection = 'B1' with exact change ", () => {
       it("it should return sprite with no change", () => {
         expect(vendingM.dispenseProduct("B1", 1.25)).toEqual("sprite");
       });
     });
-    describe("When selection = 'A2' whit exact change ", () => {
+    describe("When selection = 'A2' with exact change ", () => {
       it("it should return snicker with no change", () => {
         expect(vendingM.dispenseProduct("A2", 2)).toEqual("snicker");
       });
     });
-    // describe("When selection = 'B2' whit  changeInput='3' ", () => {
-    //   it("it should return product name and change back", () => {
-    //     expect(vendingM.dispenseProduct("B1", 3)).toEqual(
-    //       "you got a resses your change 1 quarter "
-    //     );
-    //   });
-    // });
-    // describe("When restocking a full slot B1", () => {
-    //   it("it should return product name and unit", () => {
-    //     expect(vendingM.restockProduct("B1")).toEqual(
-    //       "this slot is already full (50) "
-    //     );
-    //   });
-    // });
-    // describe("When restocking a full slot 1 dollar", () => {
-    //   it("it should return change name and quantity", () => {
-    //     expect(vendingM.restockChange("B1")).toEqual(
-    //       "this slot is already full (50)"
-    //     );
-    //   });
-    // });
+
+    describe("When selection = 'A1' with  changeInput='1' ", () => {
+      it("it should throw an error 'Sorry, not enough money 50 cents'", () => {
+        expect(vendingM.dispenseProduct("A1", 1)).toThrow(Error);
+      });
+    });
+    describe("When restocking a full slot B1", () => {
+      it("it should return product name and unit", () => {
+        expect(vendingM.restockProduct("B1")).toEqual(
+          "this slot is already full (50) "
+        );
+      });
+    });
+    describe("When restocking a full slot 1 dollar", () => {
+      it("it should return change name and quantity", () => {
+        expect(vendingM.restockChange("B1")).toEqual(
+          "this slot is already full (50)"
+        );
+      });
+    });
     describe("When restocking 1 dollar ", () => {
-      it("should throw an error 'this slot is already full (50)", () => {
+      it("should throw an error 'this slot is already full (50) for 1 dollar", () => {
         expect(() => vendingM.restockChange("1 dollar")).toThrow(Error);
       });
     });
